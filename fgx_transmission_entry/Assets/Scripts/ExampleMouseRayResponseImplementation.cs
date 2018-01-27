bbc3f3e05b81ddaeec7e1fe794a6597c62edb489
Eurybus <wraitholyth@gmail.com>
2018-01-27 13:20:24 +0200

    Mouse raycasting detection implemented.

---
+++ fgx_transmission_entry/Assets/Scripts/ExampleMouseRayResponseImplementation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleMouseRayResponseImplementation : MonoBehaviour, IMouseRayResponseGeneric
+{
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+
+	public void RayHitAction()
+	{
+		Debug.Log("Ray hit me! t. " + gameObject.name);
+	}
+}
