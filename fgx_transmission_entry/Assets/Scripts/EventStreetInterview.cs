d9329afd1f5e28f15dcf2c71da3e1a134d210c1c
Eurybus <wraitholyth@gmail.com>
2018-01-28 00:28:36 +0200

    Reworked Event scripts.

--- fgx_transmission_entry/Assets/Scripts/EventStreetInterview.cs
+++ fgx_transmission_entry/Assets/Scripts/EventStreetInterview.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class EventStreetInterview : GenericScriptedEvent, IGenericScriptedEvent {
+public class EventStreetInterview : GenericScriptedEvent, IGenericScriptedEvent
+{
 
+	private int _eventId = 0;
+
+	private StateIds _stateIds;
+
+	private int currentState;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,8 +21,59 @@ public class EventStreetInterview : GenericScriptedEvent, IGenericScriptedEvent 
 		
 	}
 
+	private void FixedUpdate()
+	{
+		switch (_stateIds)
+		{
+			case StateIds.EventStart:
+				//Use this to init scene and stuff
+				//When ready call EnterSceneOne()
+				EnterSceneOne();
+				break;
+			case StateIds.EventSceneOne:
+				//Do shit
+				//after shit is done, call EnterSceneTwo()
+				//or if first scene was failed call EnterSceneFailier()
+				EnterSceneTwo();
+				break;
+			case StateIds.EventSceneTwo:
+				//Do shit:
+				EnterSceneSuccess();
+				break;
+			case StateIds.EventSuccess:
+				//Do shit
+			case StateIds.EventFail:
+				//Do shit
+			default:
+				break;
+			
+		}
+	}
+
+	public override void EnterSceneOne()
+	{
+		base.EnterSceneOne();
+	}
+
+	public override void EnterSceneTwo()
+	{
+		base.EnterSceneTwo();
+	}
+
+	public override void EnterSceneSuccess()
+	{
+		currentState = (int) StateIds.EventFail;
+		Manager.NotifyEventSuccess(_eventId);	
+	}
+
+	public override void EnterSceneFailier()
+	{
+		currentState = (int) StateIds.EventFail;
+		Manager.NotifyEventFailier(_eventId);	
+	}
+
 	public void StartEvent()
 	{
-		throw new System.NotImplementedException();
+		currentState = (int)StateIds.EventStart;
 	}
 }
