59333f94a51591c82cd0ed4f5f9a295d599f36ec
Eurybus <wraitholyth@gmail.com>
2018-01-27 17:00:05 +0200

    Camera picture is now outputted to console.

--- fgx_transmission_entry/Assets/Scripts/GameManagerScript.cs
+++ fgx_transmission_entry/Assets/Scripts/GameManagerScript.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManagerScript : MonoBehaviour {
-
+public class GameManagerScript : MonoBehaviour
+{
+	private static GameManagerScript instance;
+	/// <summary>
+	/// This allows gameObject that this script is attached to be preserved through Scene changes
+	/// </summary>
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+			instance = this;
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
 	// Use this for initialization
 	void Start () {
 		
